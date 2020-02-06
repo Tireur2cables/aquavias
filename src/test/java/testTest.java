6e20dc6de9039b778aef6dd354935a7bf1a68269
Barrault Victor <victorbarrault@gmail.com>
2020-02-06 17:40:08 +0100

    ajout du premier test pour valider que ceux-ci fonctionnent

---
+++ src/test/java/testTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
+public class testTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+    @Test
+    public void mainShouldDislayLaunchMessage() {
+        GameLauncher.main(new String[]{"test"});
+        assertEquals("Le jeu se lance!\n", outContent.toString());
+    }
+}
