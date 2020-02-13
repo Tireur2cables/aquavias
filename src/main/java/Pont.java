8f6651207675af11fb5f027c56e9ac3488c1f345
Justin <home@openroam-prg-hf-1-133-252.net.univ-paris-diderot.fr>
2020-02-13 13:44:51 +0100

    Début de l'implémentation de calculateSorties

--- src/main/java/Pont.java
+++ src/main/java/Pont.java
@@ -7,14 +7,17 @@ public class Pont {
     private char orientation; /* N, E, S, O */
     private boolean[] sorties;
     private boolean water;
+    private String spe; /* entree, sortie, immobile */
 
     public Pont(JSONArray json){
         this.forme = json.getString(0).toUpperCase().charAt(0);
         this.orientation = json.getString(1).toUpperCase().charAt(0);
+        this.spe = json.getString(2).toLowerCase();
         this.sorties = this.calculateSorties();
         this.water = false;
     }
 
+    //constructeur voué à disparaitre
     public Pont(char forme, char orientation) {
         this.forme = forme;
         this.orientation = orientation;
@@ -23,8 +26,53 @@ public class Pont {
     }
 
     public boolean[] calculateSorties(){
-        boolean[] tab = new boolean[4];
-        return tab;
+        switch (forme) {
+          case "I" :
+            return this.calculateSortiesI();
+          case "L" :
+            return this.calculateSortiesL();
+          case "T" :
+            return this.calculateSortiesT();
+        }
+    }
+
+    private boolean[] calculateSortiesI(){
+      boolean[] tab = boolean[4];
+      for(int = 0 ; i < tab.length ; i++){
+        tab[i] = ((this.orientation == 'N' || this.orientation == 'S') && i%2 == 0)
+              || ((this.orientation == 'E' || this.orientation == 'O') && i%2 == 1);
+      }
+    }
+
+    private boolean[] calculateSortiesL(){
+      boolean[] tab = boolean[4];
+      switch (this.orientation) {
+        case 'N' :
+          tab[0] = true;
+          tab[1] = true;
+          tab[2] = false;
+          tab[3] = false;
+          return tab;
+        case 'E' :
+          tab[0] = false;
+          tab[1] = true;
+          tab[2] = true;
+          tab[3] = false;
+          return tab;
+        case 'S' :
+          tab[0] = false;
+          tab[1] = false;
+          tab[2] = true;
+          tab[3] = true;
+          return tab;
+        case 'O' :
+          tab[0] = true;
+          tab[1] = false;
+          tab[2] = false;
+          tab[3] = true;
+            return tab;
+      }
+
     }
 
     public char getForme(){
