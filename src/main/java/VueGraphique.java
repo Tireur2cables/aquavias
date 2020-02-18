d520524fb9cf84fb8250b348a5c9dfe11f70b1c5
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-02-18 02:39:10 +0100

    ajout de la fonction pour tourner une image

--- src/main/java/VueGraphique.java
+++ src/main/java/VueGraphique.java
@@ -1,4 +1,5 @@
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class VueGraphique {
         String chemin = "resources/img/" + cheminPont(c);
         BufferedImage image = chargeImage(chemin);
         View v = new View("Pont", image);
+        View v2 = new View("Pont", rotate(image, 90));
     }
 
     private static String cheminPont(char c) {
@@ -37,6 +39,19 @@ public class VueGraphique {
             System.out.println("Impossible de trouver l'image correspondant au chemin : " + chemin);
         }
         throw new RuntimeException("Erreur de chargement de l'image");
+    }
+
+    public static BufferedImage rotate(BufferedImage bimg, double angle) {
+
+        int w = bimg.getWidth();
+        int h = bimg.getHeight();
+
+        BufferedImage rotated = new BufferedImage(w, h, bimg.getType());
+        Graphics2D graphic = rotated.createGraphics();
+        graphic.rotate(Math.toRadians(angle), w/2, h/2);
+        graphic.drawImage(bimg, null, 0, 0);
+        graphic.dispose();
+        return rotated;
     }
 
 }
