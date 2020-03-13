ba66fbb898e78a0148655633128200db7cf9332a
Barrault Victor <victorbarrault@gmail.com>
2020-03-13 10:40:54 +0100

    Ajout de la classe PontIGraph pour gérer la partie graphique des ponts

---
+++ src/main/java/PontIGrap.java
@@ -0,0 +1,8 @@
+import java.awt.image.BufferedImage;
+
+public class PontIGraph extends PontI implements PontGraph{
+	static BufferedImage[] pontI = chargeImage("InoO.png");
+    static BufferedImage[] pontIEau = chargeImage("IwO.png");
+
+
+}
