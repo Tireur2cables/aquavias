d4b7f61efb08796bc2b01ccd63b8150cc94b424d
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-14 13:13:22 +0100

    je commence a refactor un peu

--- src/main/java/PontGraph.java
+++ src/main/java/PontGraph.java
@@ -1,5 +1,21 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public interface PontGraph{
 
-
+    /* FIXMe dossier image en static ? */
+    static BufferedImage chargeImage(String chemin) {
+        String dossierImages = "resources/img/";
+        chemin = dossierImages + chemin;
+        try {
+            return ImageIO.read(new File(chemin));
+        }catch (IOException e) {
+            throw new RuntimeException("Impossible de charger l'image de chemin : " + chemin);
+        }catch (NullPointerException e) {
+            throw new RuntimeException("Impossible de trouver l'image correspondant au chemin : " + chemin);
+        }
+    }
 	
 }
