bc0483dfc4d70c44281c339d6a89db442ec1f3b3
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-14 15:38:23 +0100

    retrait d'un parametre inutile

--- src/main/java/PontIGraph.java
+++ src/main/java/PontIGraph.java
@@ -4,7 +4,6 @@ public class PontIGraph extends PontGraph {
 
 	static BufferedImage[] pontI = PontGraph.chargeImages("InoO.png");
     static BufferedImage[] pontIEau = PontGraph.chargeImages("IwO.png");
-	private int orientation;
 
 	PontIGraph(char orientation) {
 		super(orientation);
