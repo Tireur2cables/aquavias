ec71c6f721b75bd973f7ba92c6b50762cbb80234
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-17 00:39:33 +0100

    reorganisation for controller

--- src/main/java/Controleur.java
+++ src/main/java/Controleur.java
@@ -3,6 +3,9 @@ class Controleur {
     private Jeu jeu;
     private VueGraphique graph;
 
+    /**
+     * INIT PART
+     */
     Controleur() {
         this.jeu = new Jeu(this);
         this.graph = new VueGraphique(this);
@@ -14,17 +17,9 @@ class Controleur {
         System.out.println("Le jeu se lance!");
     }
 
-    Pont getPont(int x, int y) {
-        return this.jeu.getPont(x, y);
-    }
-
-    int getHauteur() {
-        return this.jeu.getHauteur();
-    }
-
-    int getLargeur() {
-        return this.jeu.getLargeur();
-    }
+    /**
+     * REQUETTE PART
+     * */
 
     void tournePont(int x, int y) {
         /* change les sorties du pont et l'orientation */
@@ -51,18 +46,6 @@ class Controleur {
             this.graph.decrementeProgressBar();
     }
 
-    boolean isMovable(int x, int y) {
-        return this.jeu.isMovable(x, y);
-    }
-
-    void setEau(int x, int y, boolean eau) {
-        this.graph.setEau(x, y ,eau);
-    }
-
-    void exportNiveau(int number, boolean nouveauNiveau) {
-        this.jeu.exportNiveau(number, nouveauNiveau);
-    }
-
     void initTimer() {
         this.jeu.initTimer();
     }
@@ -77,14 +60,6 @@ class Controleur {
     void defaite() {
         this.graph.defaite();
         this.jeu.stopTimer();
-    }
-
-    String getMode() {
-        return this.jeu.getMode();
-    }
-
-    int getLimite() {
-        return this.jeu.getLimite();
     }
 
     /**
@@ -106,6 +81,38 @@ class Controleur {
      */
     void nextLevel() {
         System.out.println("Niveau suivant");
+    }
+
+    Pont getPont(int x, int y) {
+        return this.jeu.getPont(x, y);
+    }
+
+    int getHauteur() {
+        return this.jeu.getHauteur();
+    }
+
+    int getLargeur() {
+        return this.jeu.getLargeur();
+    }
+
+    boolean isMovable(int x, int y) {
+        return this.jeu.isMovable(x, y);
+    }
+
+    String getMode() {
+        return this.jeu.getMode();
+    }
+
+    int getLimite() {
+        return this.jeu.getLimite();
+    }
+
+    void setEau(int x, int y, boolean eau) {
+        this.graph.setEau(x, y ,eau);
+    }
+
+    void exportNiveau(int number, boolean nouveauNiveau) {
+        this.jeu.exportNiveau(number, nouveauNiveau);
     }
 
 }
