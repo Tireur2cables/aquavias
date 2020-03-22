7d570020d1e859df24f20c1349e530f36043b00c
Barrault Victor <victorbarrault@gmail.com>
2020-03-22 11:20:07 +0100

    test sur l'initialisation d'un niveau

--- src/test/java/JeuTest.java
+++ src/test/java/JeuTest.java
@@ -7,16 +7,23 @@ public class JeuTest {
     @Test
     public void initNiveauIsWorkingGood(){
         Jeu testJeu = new Jeu(new Controleur());
-        testJeu.initNiveau(2);
+        testJeu.initNiveau(1);
         assertEquals(testJeu.getHauteur(), 3);
-        assertEquals(testJeu.getLargeur(), 4);
+        assertEquals(testJeu.getLargeur(), 3);
         assertEquals(testJeu.getMode(), "compteur");
         assertEquals(testJeu.getLimite(), 10);
-        assert(testJeu.getDebit() == 1);
-        assertEquals(testJeu.getCompteur(), 10);
+        assertEquals(testJeu.getDebit(), 1, 0);
+        assertEquals(testJeu.getCompteur(), 10, 0);
+        assert(testJeu.isEntree(0,1));
+        assert(testJeu.isSortie(2,1));
+        assert(!testJeu.isMovable(0,1));
+        assert(!testJeu.isMovable(2,1));
         assert(testJeu.getPont(0, 1).isEntree());
-        assert(testJeu.getPont(3, 1).isSortie());
+        assert(testJeu.getPont(2, 1).isSortie());
         assert(!testJeu.getPont(0, 1).isMovable());
-        assert(!testJeu.getPont(3, 1).isMovable());
+        assert(!testJeu.getPont(2, 1).isMovable());
+
+
+
     }
 }
