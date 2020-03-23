c51f9e9b754aa125a2654ffd11e8331cfa2eedf4
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-23 19:33:20 +0100

    ajout des fonctions d'écriture de json dans genNiveaux

--- GenNiveaux/src/main/java/GenNiveaux.java
+++ GenNiveaux/src/main/java/GenNiveaux.java
@@ -1,7 +1,71 @@
-public class GenNiveaux {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collection;
+
+/* Import with maven dependencies */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+class GenNiveaux {
+
+    private static String exportDir = "../resources/niveaux/niveau";
 
     public static void main(String[] args) {
         System.out.println("je suis gen niveau");
     }
+
+    /*
+
+    private static void exportNiveau() {
+        String chemin = exportDir + numNiveau + ".json";
+        JSONObject fic = this.createJSON();
+        writeFile(fic, chemin);
+    }
+
+    private static JSONObject createJSON() {
+        JSONObject fic = new JSONObject();
+        fic.put("num", this.numNiveau);
+        fic.put("hauteur", this.getHauteur());
+        fic.put("largeur", this.getLargeur());
+        fic.put("mode", this.mode);
+        fic.put("limite", this.limite);
+        fic.put("compteur", this.compteur);
+        JSONArray niveau = this.saveNiveau();
+        fic.put("niveau", niveau);
+        return fic;
+    }
+
+    private static JSONArray saveNiveau() {
+        JSONArray niveau = new JSONArray();
+        for (int i = 0; i < this.getLargeur(); i++) {
+            JSONArray ligne = new JSONArray();
+            for (int j = 0; j < this.getHauteur(); j++) {
+                Pont modPont = this.getPont(i,j);
+                if (modPont != null) {
+                    JSONArray pont = new JSONArray();
+                    pont.put((modPont.forme + ""));
+                    pont.put(modPont.orientation + "");
+                    pont.put(modPont.spe);
+                    ligne.put(pont);
+                }else
+                    ligne.put((Collection<?>) null);
+            }
+            niveau.put(ligne);
+        }
+        return niveau;
+    }
+
+    */
+
+    private static void writeFile(JSONObject file, String chemin) {
+        try {
+            FileWriter fichier = new FileWriter(chemin);
+            fichier.write(file.toString());
+            fichier.close();
+        }catch (IOException e) {
+            throw new RuntimeException("Erreur d'écriture du fichier exporté");
+        }
+    }
+
 
 }
