bd01167dd957a61ba00d4c5071c1dc4ac3f76f65
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-26 20:15:26 +0100

    ajout de quelques fonctions de plateau

--- GenNiveaux/src/main/java/aquavias/generateur/Plateau.java
+++ GenNiveaux/src/main/java/aquavias/generateur/Plateau.java
@@ -11,29 +11,35 @@ class Plateau {
 
     private Pont[][] plateau;
 
+    /**
+     * INIT PART
+     * */
+
     Plateau(int largeur, int hauteur) {
         this.plateau = new Pont[largeur][hauteur];
         for (int i = 0; i < largeur; i++) {
             for (int j = 0; j < hauteur; j++) {
-                this.plateau[i][j] = this.createPont();
+                this.plateau[i][j] = this.createPont(null);
             }
         }
+        this.placerEntreeSortie();
     }
 
-    private Pont createPont() {
-        char forme = this.chooseOrientation();
+    private Pont createPont(String spe) {
+        char forme = this.chooseForme();
+        char orientation = this.chooseOrientation();
         switch(forme) {
             case 'I' :
-                return new PontI();
+                return new PontI(orientation, spe);
             case 'L' :
-                return new PontL();
+                return new PontL(orientation, spe);
             case 'T' :
-                return new PontT();
+                return new PontT(orientation, spe);
         }
         throw new RuntimeException("char du pont inconnu");
     }
 
-    private char chooseOrientation() {
+    private char chooseForme() {
         int random = ThreadLocalRandom.current().nextInt(0, 3);
         switch (random) {
             case 0 :
@@ -44,6 +50,44 @@ class Plateau {
                 return 'T';
         }
         throw new RuntimeException("Random int out of bounds");
+    }
+
+    private char chooseOrientation() {
+        int random = ThreadLocalRandom.current().nextInt(0, 4);
+        switch (random) {
+            case 0 :
+                return 'N';
+            case 1 :
+                return 'E';
+            case 2 :
+                return 'S';
+            case 3 :
+                return 'O';
+        }
+        throw new RuntimeException("Random int out of bounds");
+    }
+
+    private void placerEntreeSortie() {
+        int i = ThreadLocalRandom.current().nextInt(0,this.getLargeur());
+        int j = ThreadLocalRandom.current().nextInt(0, this.getHauteur());
+        this.plateau[i][j] = this.createPont("Entree");
+        this.plateau[i][j] = this.createPont("Sortie");
+    }
+
+    /**
+     * GETTEUR PART
+     */
+
+    Pont[][] getPlateau() {
+        return this.plateau;
+    }
+
+    private int getLargeur() {
+        return this.plateau.length;
+    }
+
+    private int getHauteur() {
+        return this.plateau[0].length;
     }
 
 }
