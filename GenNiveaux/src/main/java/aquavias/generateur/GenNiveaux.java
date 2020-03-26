9f4259dda80b27f8e6cf55fa4f4d67882a0715fa
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-03-26 20:23:24 +0100

    début d'écriture d'un jeu dans genNiveau

--- GenNiveaux/src/main/java/aquavias/generateur/GenNiveaux.java
+++ GenNiveaux/src/main/java/aquavias/generateur/GenNiveaux.java
@@ -1,5 +1,6 @@
 package aquavias.generateur;
 
+import aquavias.jeu.Jeu;
 import aquavias.jeu.Pont;
 
 import java.io.FileWriter;
@@ -14,6 +15,11 @@ class GenNiveaux {
 
     public static void main(String[] args) {
         System.out.println("je suis gen niveau");
+    }
+
+    private Jeu createJeu(int largeur, int hauteur) {
+        Plateau p = new Plateau(largeur, hauteur);
+        return new Jeu();
     }
 
 }
