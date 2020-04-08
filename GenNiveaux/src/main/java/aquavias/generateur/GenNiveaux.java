089745f17d9b39d9f1f4b8f3e66cad0f99d98e50
Barrault Victor <victorbarrault@gmail.com>
2020-04-08 16:10:19 +0200

    classe generateur static
    la classe generateur est maintenant entièrement static et la génération du plateau se déroulera dans la classe Plateau

--- GenNiveaux/src/main/java/aquavias/generateur/GenNiveaux.java
+++ GenNiveaux/src/main/java/aquavias/generateur/GenNiveaux.java
@@ -8,22 +8,30 @@ class GenNiveaux {
 
     public static void main(String[] args) {
         System.out.println("gen niveau -> WIP cf. branche GenererNiveaux");
+        System.out.println("Pour le moment, le niveau est entièrement générer au hasard");
+        exportNiveau(chooseDim(), chooseDim(), 15);
+
     }
 
-    private void exportNiveau(int largeur, int hauteur, int numNiveau) {
+    private static void exportNiveau(int largeur, int hauteur, int numNiveau) {
         Plateau p = new Plateau(largeur, hauteur);
-        String mode = this.chooseMode();
-        int limite = this.chooseLimite();
+        String mode = chooseMode();
+        int limite = chooseLimite();
         Jeu jeu = new Jeu(p.getPlateau(), numNiveau, mode, limite);
         jeu.exportNiveau();
     }
 
-    private String chooseMode() {
+    private static String chooseMode() {
         return (ThreadLocalRandom.current().nextBoolean())? "compteur" : "fuite";
     }
 
-    private int chooseLimite() {
+    private static int chooseLimite() {
         return 100;
+    }
+
+    private static int chooseDim(){
+        return 4;
+        //return ThreadLocalRandom.current().nextInt(3, 10);
     }
 
 }
